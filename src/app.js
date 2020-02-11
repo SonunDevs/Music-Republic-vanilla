023e92c865321beea11eec60318ef17847b6e99f
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-02-11 21:36:26 +0600

    added Axios

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import "./styles.css";
 import "./player/player";
+import axios from "./axios/axios";
+
+document.documentElement.addEventListener("click", () => {
+  axios
+    .get("/playlist.json")
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
---
+++ src/axios/axios.js
@@ -0,0 +1,5 @@
+const instance = axios.create({
+  baseURL: "https://music-republic-9aa07.firebaseio.com/"
+});
+
+export default instance;
