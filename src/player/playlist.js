12094ea65a380a229e7b4a34fa0957cea019da9a
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-02-11 20:36:43 +0600

    moved "var playlist" to another file

---
+++ src/player/playlist.js
@@ -0,0 +1,35 @@
+export var playlist = [
+  {
+    song: "Forever",
+    album: "Divinely Uninspired To A Hellish Extent",
+    artist: "Lewis Capaldi",
+    artwork:
+      "https://cdn.hmv.com/r/w-3280/getattachment/18c674a8-c155-4722-adba-6676988f292b/mus000607312-1.jpg",
+    mp3: "https://download.mp3-here.icu/c/Lewis-Capaldi-Forever.mp3"
+  },
+  {
+    song: "Eastside",
+    album: "Eastside",
+    artist: "Benny Blanco feat.Khaled & Halsey",
+    artwork: "https://upload.wikimedia.org/wikipedia/ru/2/2e/Eastside.jpg",
+    mp3:
+      "http://zaycevfm.net/mp3/zarubezhnaya_2/benny-blanco-halsey-eastside-feat-khalid_(zaycevfm_net).mp3"
+  },
+  {
+    song: "Girls like you",
+    album: "Girls like you",
+    artist: "Maroon 5",
+    artwork:
+      "https://m.media-amazon.com/images/M/MV5BNmNmNGExNDctM2Q3MC00MTU5LTkyYzMtMzMyYmI2MTA3MjU3XkEyXkFqcGdeQXVyNjE0ODc0MDc@._V1_.jpg",
+    mp3:
+      "https://static.muzlo.cc/download/24717/Maroon-5-Cardi-B_-_Girls-Like-You.mp3"
+  },
+  {
+    song: "Night Witches",
+    album: "Heroes",
+    artist: "Sabaton",
+    artwork:
+      "https://avatars.yandex.net/get-music-content/33216/f0f4e535.a.1971440-1/m1000x1000",
+    mp3: "https://download.mp3-here.icu/e/Sabaton-Night-Witches.mp3"
+  }
+];
