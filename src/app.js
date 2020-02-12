b581224438736b302c31e002e4047b9cc5da2828
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-02-12 18:20:23 +0600

    deleted axios

--- src/app.js
+++ src/app.js
@@ -1,24 +1,2 @@
 import "./styles.css";
 import "./player/player";
-import axios from "./axios/axios";
-
-document.documentElement.addEventListener("click", () => {
-  axios
-    .get("/playlist.json")
-    .then(response => {
-      // преобразование объекта в массив
-      let arr = [];
-      for (const key in response.data) {
-        if (response.data.hasOwnProperty(key)) {
-          const element = response.data[key];
-          arr.push(element);
-        }
-      }
-
-      // console.log(response.data); // response.data - полученные данные от запроса
-      console.log(arr);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
--- src/axios/axios.js
+++
@@ -1,5 +0,0 @@
-const instance = axios.create({
-  baseURL: "https://music-republic-9aa07.firebaseio.com/"
-});
-
-export default instance;
