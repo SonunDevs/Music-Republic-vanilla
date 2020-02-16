6fa8106be540ae61812ff453ba856ab600c1141b
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-02-16 16:30:52 +0600

    fixed link bugs in modal windows

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,6 +1,8 @@
 let backdropState = false;
 let btnLogin = document.querySelector("#login");
 let btnSignup = document.querySelector("#signup");
+let linkLogin = document.querySelector(".link_login");
+let linkSignup = document.querySelector(".link_signup");
 let backdrop = document.querySelector(".backdrop");
 let login = document.querySelector(".modal_win_login");
 let signup = document.querySelector(".modal_win_signup");
@@ -15,6 +17,14 @@ btnLogin.addEventListener("click", function() {
 btnSignup.addEventListener("click", function() {
   showingBackdrop();
   showingModals("signup");
+});
+
+linkLogin.addEventListener("click", function() {
+  showingModals("signup", true);
+});
+
+linkSignup.addEventListener("click", function() {
+  showingModals("login", true);
 });
 
 backdrop.addEventListener("click", event => {
@@ -33,8 +43,8 @@ function showingBackdrop() {
   }
 }
 
-function showingModals(modalWindow) {
-  if (backdropState) {
+function showingModals(modalWindow, linkClicked) {
+  if (backdropState || linkClicked) {
     if (modalWindow == "signup") {
       signup.className = "modal_win modal_win_signup active";
       login.className = "modal_win modal_win_login";
